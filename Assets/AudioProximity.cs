462faef953a8077b041b95843315063bc14a1003
martin rabaglia <martinrabaglia@martins-MacBook-Pro.local>
2015-09-22 22:00:51 -0300

    mezcla rara

---
+++ Assets/AudioProximity.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioProximity : MonoBehaviour {
+
+
+	public GameObject player;
+	public float playerDistance;
+	public GameObject target;
+	public AudioSource gameAudio;
+	public bool canPlayAudio = true;
+	
+	void Update() {
+		
+		playerDistance = Vector3.Distance(player.transform.position, 
+		                 new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z));
+		if (playerDistance < 4) {
+			if (canPlayAudio) {
+//				gameAudio.clip = gameAudio;
+				gameAudio.volume = 0.5f; // adjust the volume to whatever you want
+				gameAudio.Play();
+				canPlayAudio = false;  // you can reset this in your code elsewhere... I did this just to make sure the audio only plays once.
+			}
+		}
+	}
+//	
+//	IEnumerator AdjustVolume () {
+//		
+//		while(true) {
+//			
+//			if(audio.isPlaying) { // do this only if some audio is being played in this gameObject's AudioSource
+//				
+//				float distanceToTarget = Vector3.Distance(transform.position, target.position); // Assuming that the target is the player or the audio listener
+//				
+//				if(distanceToTarget < 1) { distanceToTarget = 1; }
+//				
+//				audio.volume = 1/distanceToTarget; // this works as a linear function, while the 3D sound works like a logarithmic function, so the effect will be a little different (correct me if I'm wrong)
+//				
+//				yield return new WaitForSeconds(1); // this will adjust the volume based on distance every 1 second (Obviously, You can reduce this to a lower value if you want more updates per second)
+//				
+//			}
+//		}
+//		
+//	}
+
+}
+
---
+++ Assets/AutoMove.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoMove : MonoBehaviour {
+
+	public GameObject character;
+
+	public GameObject[] targets;
+	public float[] durations;
+	public float[] delays;
+	public AudioSource[] sources;
+
+	private Vector3 endPoint;
+	public int Current = 0;
+
+	private float freezedTime = 0;
+	private float toWait =0;
+	
+	private Vector3 startPoint;
+	private float startTime;
+
+	private bool pleaseStop = false;
+	
+	void Start() {
+		startPoint = character.transform.position;
+		startTime = Time.time;
+	}
+	
+	
+	void Update(){
+
+		var endPoint = targets[Current].transform.position;
+		var duration = durations[Current];
+		var delayMore = delays[Current];
+		var audioNow = sources[Current];
+		if (Time.time < startTime + duration + delayMore && !pleaseStop) {
+
+			character.transform.position = 
+				Vector3.Lerp (startPoint, endPoint, (Time.time - startTime) / duration);
+			if (audioNow != null){
+				if (!audioNow.isPlaying){
+					audioNow.Play();
+				}
+			}
+		} else {
+
+			if (Current + 1 < targets.Length) {
+				startTime = Time.time;
+				startPoint = endPoint;
+				Current++;
+				Debug.Log(Current);
+			} else {
+				pleaseStop = true;
+			}
+		}
+
+
+	}
+}
